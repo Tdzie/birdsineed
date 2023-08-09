09297ba01bf38850504bbb326c9b0be5453ee30b
marsh000111 <65301716+marsh000111@users.noreply.github.com>
2023-08-08 21:48:04 -0400

    updates and fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,65 @@
+const SPREADSHEET_URL = 'https://script.google.com/macros/s/AKfycbxdvaBuBfRlwScEL1YLe_EUB8YzEMmkz_aaky7S6DPbT3Y_5-hI_KSL2OWcerfhAIEx/exec';
 
-    // Replace this with your spreadsheet's URL in the JSON format
-const SPREADSHEET_URL = 'https://spreadsheets.google.com/feeds/list/1-N4bTDRRTT2liS2tYaIPCqq490ik_BaR_WtzbxDd8uA/od6/public/values?alt=json';
+let database = [];
 
-    // Function to fetch spreadsheet data
-    function fetchSpreadsheetData() {
-        fetch(SPREADSHEET_URL)
-            .then(response => response.json())
-            .then(data => {
-                // For demonstration purposes, just stringify the data and display it.
-                // In a real application, you'd want to process and display this data more thoughtfully.
-                document.getElementById('spreadsheetData').innerText = JSON.stringify(data, null, 2);
-            })
-            .catch(error => {
-                console.error('Error fetching spreadsheet data:', error);
-                document.getElementById('spreadsheetData').innerText = 'Error fetching data. See console for details.';
+// Function to fetch spreadsheet data
+function fetchSpreadsheetData() {
+    fetch(SPREADSHEET_URL)
+        .then(response => response.json())
+        .then(data => {
+            database = data;  // Store the fetched database data for further processing
+            fetchEBirdData();  // Fetch eBird data after we have the database data
+           // console.log('Spreadsheet data:', data);
+        })
+        .catch(error => {
+            console.error('Error fetching spreadsheet data:', error);
+        });
+}
+
+const eBirdApiToken = '377m29pfd648';
+
+function fetchEBirdData() {
+    const myHeaders = new Headers();
+    myHeaders.append("X-eBirdApiToken", eBirdApiToken);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    const lat = `36.07`;
+    const lng = `-115.09`;
+
+    fetch(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${lat}&lng=${lng}&back=1&dist=50&includeProvisional=true&sort=species`, requestOptions)
+        .then(response => response.json())
+        .then(eBirdData => {
+            let newBirdsForProcessing = [];
+
+            eBirdData.forEach(bird => {
+                let found = false;
+                for (let dbEntry of database) {
+                    if (bird.sciName === dbEntry[3]) {
+                        
+                        if (dbEntry[0] === "X" || dbEntry[0] === "x") {
+                          found = true;  
+                        }
+                    }
+                }
+                if (!found) {
+                    newBirdsForProcessing.push(bird);
+                }
             });
-    }
 
-    // Fetch data when the page loads
-    window.addEventListener('load', fetchSpreadsheetData);
+            console.log(newBirdsForProcessing);
+            //document.getElementById('spreadsheetData').innerText = JSON.stringify(eBirdData, null, 2);
+            document.getElementById('dataDisplay').innerText = JSON.stringify(newBirdsForProcessing, null, 2);
+        })
+        .catch(error => {
+            console.log('error', error);
+            document.getElementById('dataDisplay').innerText = 'Error fetching eBird data. See console for details.';
+        });
+}
+
+// Fetch spreadsheet data when the page loads
+window.addEventListener('load', fetchSpreadsheetData);
